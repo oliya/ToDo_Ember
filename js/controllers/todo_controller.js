91b36cb5554f26e407adf878b48e759bb9ffa9ee
oliya <oliya@2359media.com>
2013-07-22 01:05:45 +0800

    initial commit
    accepting edits:
    
    http://emberjs.com/guides/getting-started/accepting-edits/

---
+++ js/controllers/todo_controller.js
@@ -0,0 +1,23 @@
+Todos.TodoController = Ember.ObjectController.extend({
+
+  isEditing: false,
+
+editTodo: function () {
+  this.set('isEditing', true);
+},
+
+
+  isCompleted: function(key, value){
+    var model = this.get('model');
+
+    if (value === undefined) {
+      // property being used as a getter
+      return model.get('isCompleted');
+    } else {
+      // property being used as a setter
+      model.set('isCompleted', value);
+      model.save();
+      return value;
+    }
+  }.property('model.isCompleted')
+});
