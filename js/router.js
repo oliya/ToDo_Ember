de91e26edadc6850222e86ef4d17d1009b8e90d1
oliya <oliya@2359media.com>
2013-07-22 18:12:47 +0800

    Revert "Revert "adding 'completed'""
    This reverts commit 078218bde4ba768c7f19190eaca4b2050b3e69ad.

--- js/router.js
+++ js/router.js
@@ -1,10 +1,28 @@
+
 Todos.Router.map(function () {
-  this.resource('todos', { path: '/' });
+  this.resource('todos', { path: '/' }, function () {
+    // additional child routes
+
+        this.route('active');
+
+  });
+});
+
+Todos.TodosIndexRoute = Ember.Route.extend({
+  model: function () {
+    return Todos.Todo.find();
+  }
 });
 
 
-Todos.TodosRoute = Ember.Route.extend({
-  model: function () {
-    return Todos.Todo.find();
+
+Todos.TodosActiveRoute = Ember.Route.extend({
+  model: function(){
+    return Todos.Todo.filter(function (todo) {
+      if (!todo.get('isCompleted')) { return true; }
+    });
+  },
+  renderTemplate: function(controller){
+    this.render('todos/index', {controller: controller});
   }
 });
